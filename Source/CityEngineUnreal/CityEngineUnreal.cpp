3b27a52f29b6a7850fe4446e38143b7add153311
TNMNRamboll <146937899+TNMNRamboll@users.noreply.github.com>
2024-01-29 12:22:25 +0100

    Rest of files

---
+++ Source/CityEngineUnreal/CityEngineUnreal.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CityEngineUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CityEngineUnreal, "CityEngineUnreal" );
